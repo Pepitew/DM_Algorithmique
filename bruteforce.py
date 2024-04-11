c4fa4dc5173c221f468b83a8d8d2624d1034b7ea
Pepitew <clementbessieres.pro@gmail.com>
2024-04-11 22:34:18 +0200

    all

--- bruteforce.py
+++ bruteforce.py
@@ -1,17 +1,12 @@
 solution = []
-def contraintes_unicités(sol):
-    for i in range(10):
-    if(sol[i] != sol[i+1] and
-       sol[i] != sol[2] and
-       sol[i] != sol[3] and
-       sol[i] != sol[4] and
-       sol[i] != sol[5] and
-       sol[i] != sol[6] and
-       sol[i] != sol[7] and
-       sol[i] != sol[8] and
-       sol[i] != sol[9] and
-       ):
-def contraintes(sol):
+
+def tous_differents(sol):
+    for i in range(len(sol)):
+        for j in range(len(sol)):
+            if(sol[i] == sol[j]):
+                return False
+            
+def calcul(sol):
     if ( (sol[0]*1000 + sol[1]*100 +sol[2]*10 + sol[3]) 
        + (sol[5]*1000 + sol[6]*100 + sol[7]*10 + sol[8])
        == (sol[10]*10000 + sol[11]*1000 + sol[12]*100 + sol[13]*10 + sol[14])
@@ -20,7 +15,7 @@ def contraintes(sol):
 
 def brutality():
     
-    #! coca + cola + pepsi
+    #! coca + cola = pepsi
     #?caractère unique : c o a l p e s i
     
     for c in range(10):
@@ -32,7 +27,7 @@ def brutality():
                             for s in range(10):
                                 for i in range(10):
                                     print(c,o,c,a,"|",c,o,l,a,"|",p,e,p,s,i)
-                                    if contraintes([c,o,c,a,"|",c,o,l,a,"|",p,e,p,s,i]):
+                                    if calcul([c,o,c,a,"|",c,o,l,a,"|",p,e,p,s,i]) and tous_differents([c,o,a,l,p,e,s,i]):
                                         solution.append([c,o,c,a,"|",c,o,l,a,"|",p,e,p,s,i])
 
 def exec_brutality():
